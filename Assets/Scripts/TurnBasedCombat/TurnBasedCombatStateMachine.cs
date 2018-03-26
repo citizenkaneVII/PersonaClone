894c466a2e0c079aeb93af18bce375c5bb871e37
Unknown <citizenkaneVII@gmail.com>
2018-03-26 12:17:07 -0600

    Start BattleManager

---
+++ Assets/Scripts/TurnBasedCombat/TurnBasedCombatStateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedCombatStateMAchine : MonoBehaviour {
+
+	public enum BattleStates{
+		START,
+		PLAYERCHOICE,
+		ENEMYCHOICE,
+		LOSE,
+		WIN,
+	}
+
+	private BattleStates currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = BattleStates.START;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
